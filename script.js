97003c706b8ff1eaad1357af342c912e93ac5e3e
AlenaMikalaeynaLisava <alenalisava87@gmail.com>
2020-10-16 16:19:58 +0300

    fix: change selector for image

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const image = document.querySelector(".pretty-image");
+const image = document.querySelector("img");
 window.addEventListener("keydown", (event) => {
     if (event.code === "Space") {
         image.classList.toggle("pretty-image");
